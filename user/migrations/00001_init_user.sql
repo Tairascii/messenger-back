fd64c4adb26184f96c4231c10e793efa25bff817
Tair Sayranbekov <tairtopskiy@gmail.com>
2025-08-19 21:03:57 +0500

    db

---
+++ user/migrations/00001_init_user.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+-- +goose StatementBegin
+create table users (
+    id uuid primary key not null default gen_random_uuid(),
+    email varchar(255) not null unique,
+    password text not null,
+    created_at timestampt default now()
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+drop table users;
+-- +goose StatementEnd
